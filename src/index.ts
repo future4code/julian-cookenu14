0da131cd28688a78f7046752e3fd8772ec48f367
Murilo Oliari Ribeiro <murilo.oliari@gmail.com>
2020-07-16 14:21:19 -0300

    app.get user/profile

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/signup', async (req: Request, res: Response) => {
+app.post("/signup", async (req: Request, res: Response) => {
     try {
         if(!req.body.email || req.body.email.indexOf('@') === -1) {
             throw new Error('Invalid Email');
@@ -79,6 +79,27 @@ app.post("/login", async (req: Request, res: Response) => {
             message: error.message,
         });
     };
+});
+
+app.get("/user/profile", async (req: Request, res: Response) => {
+    try {
+        const token = req.headers.authorization as string;
+
+        const authenticator = new Authenticator();
+        const authenticationData = authenticator.getData(token);
+
+        const userDb = new UserDatabase();
+        const user = await userDb.getById(authenticationData.id);
+
+        res.status(200).send({
+            id: user.id,
+            email: user.email,
+        });
+    } catch (err) {
+        res.status(400).send({
+            message: err.message,
+        });
+    }
 });
 
 
